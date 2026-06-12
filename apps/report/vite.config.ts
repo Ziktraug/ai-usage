df83d2f44a38abbd3d4531eedf4dc500119373f8
Nathan <95257815+Ziktraug@users.noreply.github.com>
2026-06-12 12:03:51 +0200

    package update

--- apps/report/vite.config.ts
+++ apps/report/vite.config.ts
@@ -73,10 +73,5 @@ export default defineConfig({
   build: {
     assetsInlineLimit: 100_000_000,
     cssCodeSplit: false,
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-      },
-    },
   },
 });
